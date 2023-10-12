ad5c668b97489a54023403f7f7816511843c8473
kenanoglu <evren.kenanoglu@infineon.com>
2023-10-12 17:54:13 +0200

    improve dependencies
    - Add source, binary and subbuild dir to have more control on
    dependencies

--- FetchContentDependencies.cmake
+++ FetchContentDependencies.cmake
@@ -1,18 +1,30 @@
 include(FetchContent)
 
-# Google Test Framework
+# Set Dependency Directory as a common base
+set(DEPENDENCIES_DIR "${WORKSPACE_DIR}/_deps")
 
+########################################################################################
+# Google Test Framework
+########################################################################################
 # Set the runtime library to use
 # These lines tell Google Test to use the dynamic release version of the runtime library (/MD) and to match your project's runtime library settings.
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 set(gtest_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL" CACHE STRING "MD" FORCE)
 
+# Set gtest git tag as a variable
+set(GTEST_GIT_TAG release-1.10.0) # Replace with the release tag you want to use
+
+# Set the source and build directories
+set(GTEST_SOURCE_DIR "${DEPENDENCIES_DIR}/googletest-${GTEST_GIT_TAG}-src")
+set(GTEST_BINARY_DIR "${DEPENDENCIES_DIR}/googletest-${GTEST_GIT_TAG}-build")
+set(GTEST_SUBBUILD_DIR "${DEPENDENCIES_DIR}/googletest-${GTEST_GIT_TAG}-subbuild")
+
 FetchContent_Declare(
   googletest
   GIT_REPOSITORY https://github.com/google/googletest.git
-  GIT_TAG        release-1.10.0 # Replace with the release tag you want to use
-  SOURCE_DIR     "${EMBEDDED_SYSTEM_DIR}/_deps/googletest-src"
-  BINARY_DIR     "${EMBEDDED_SYSTEM_DIR}/_deps/googletest-build"
+  GIT_TAG ${GTEST_GIT_TAG}
+  SOURCE_DIR ${GTEST_SOURCE_DIR}
+  BINARY_DIR ${GTEST_BINARY_DIR}
+  SUBBUILD_DIR ${GTEST_SUBBUILD_DIR}
 )
-
 FetchContent_MakeAvailable(googletest)
