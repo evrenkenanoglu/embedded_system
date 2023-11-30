bf560e52d67e97fd551e4224b05cfc73e3e0194b
kenanoglu <evren.kenanoglu@infineon.com>
2023-11-30 16:29:00 +0100

    Add: IProcess abstract class

---
+++ Process/IProcess.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file IProcess.hpp
+ * @brief Header file for IProcess
+ *
+ * This file contains declarations for the IProcess class and related data types and functions.
+ */
+
+#ifndef IPROCESS_HPP
+#define IPROCESS_HPP
+
+#include "System/system.h"
+
+class IProcess
+{
+public:
+    enum class State
+    {
+        INITIALIZED,
+        RUNNING,
+        PAUSED,
+        STOPPED,
+    };
+
+    virtual ~IProcess()
+    {
+    }
+
+    virtual sys_error_t start()  = 0;
+    virtual sys_error_t stop()   = 0;
+    virtual sys_error_t pause()  = 0;
+    virtual sys_error_t resume() = 0;
+    State               getState()
+    {
+        return _state;
+    }
+    void setState(State state)
+    {
+        _state = state;
+    }
+
+private:
+    State _state;
+};
+
+#endif /* IPROCESS_HPP */
--- Scripts/create_CPP_HAL_files.py
+++ Scripts/create_CPP_HAL_files.py
@@ -13,9 +13,9 @@ header_template = """\
 #ifndef {filename_upper}_HPP
 #define {filename_upper}_HPP
 
-#include "{ihal_header_file}"
+#include "{abstract_header_file}"
 
-class {classname} : public {ihal_classname} 
+class {classname} : public {abstract_classname} 
 {{
 private:
     // private members
@@ -39,6 +39,10 @@ public:
     {cpx_get_definition}
     {cpx_set_definition}
     {stop_definition}
+    {iprocess_start_definition} 
+    {iprocess_stop_definition}  
+    {iprocess_pause_definition} 
+    {iprocess_resume_definition}
 
 }};
 
@@ -53,7 +57,7 @@ source_template = """\
  * This file contains definitions for the {filename} class and related data types and functions.
  */
 
-#include "IHal.hpp"
+#include "{classname}.hpp"
 
 {classname}::{classname}() 
 {{
@@ -80,13 +84,17 @@ source_template = """\
 {cpx_get_definition}
 {cpx_set_definition}
 {stop_definition}
+{iprocess_start_definition} 
+{iprocess_stop_definition}  
+{iprocess_pause_definition} 
+{iprocess_resume_definition}
 """
 
 # Get the file name from the user
 filename = input("Enter the file name: ")
 
 # Get the HAL type from the user
-hal_type = input("Enter the HAL type (IO, COM, MEM, CPX): ").upper()
+hal_type = input("Enter the System Type (IO, COM, MEM, CPX, PROC): ").upper()
 
 if  hal_type != "":
     filename = hal_type.lower() + "_" + filename
@@ -97,6 +105,18 @@ classname = filename
 # Get the brief description from the user
 brief = input("Enter a brief description: ")
 
+abstract_header_file = ""
+abstract_classname = ""
+
+# Process Definitions
+iprocess_header_file = ""
+iprocess_classname = ""
+iprocess_start_definition = ""
+iprocess_stop_definition = ""
+iprocess_pause_definition = ""
+iprocess_resume_definition = ""
+
+# HAL Definitions
 ihal_header_file = ""
 ihal_classname = ""
 
@@ -126,21 +146,21 @@ cpx_set_definition= ""
 stop_definition= ""
 
 if hal_type == "IO":
-    ihal_header_file    = "ihal_io.h"
+    ihal_header_file    = "IHal_io.h"
     ihal_classname      = "IHAL_IO"
-    get_definition      = "void "    + classname  + "::get(void* data) override;\n"
-    set_definition      = "error_t " + classname  + "::set(void* data) override;\n"
+    get_definition      = "void "           + classname  + "::get(void* data) override;\n"
+    set_definition      = "sys_error_t "    + classname  + "::set(void* data) override;\n"
 
 elif hal_type == "COM":
-    ihal_header_file        = "ihal_com.h"
+    ihal_header_file        = "IHal_com.h"
     ihal_classname          = "IHAL_COM"
-    connect_definition      = "error_t "    + classname + "::connect() override;\n"
-    disconnect_definition   = "void "       + classname + "::disconnect() override;\n"
-    sendData_definition     = "error_t "    + classname + "::sendData(const uint8_t* data, size_t length) override;\n"
-    receiveData_definition  = "error_t "    + classname + "::receiveData(uint8_t* data, size_t maxLength, size_t& receivedLength) override;\n"
+    connect_definition      = "sys_error_t "    + classname + "::connect() override;\n"
+    disconnect_definition   = "void "           + classname + "::disconnect() override;\n"
+    sendData_definition     = "sys_error_t "    + classname + "::sendData(const uint8_t* data, size_t length) override;\n"
+    receiveData_definition  = "sys_error_t "    + classname + "::receiveData(uint8_t* data, size_t maxLength, size_t& receivedLength) override;\n"
 
 elif hal_type == "MEM":
-    ihal_header_file        = "ihal_mem.h"
+    ihal_header_file        = "IHal_mem.h"
     ihal_classname          = "IHAL_MEM"
     initialize_definition   = "bool "   + classname + "::initialize() override;\n"
     readData_definition     = "bool "   + classname + "::readData(uint32_t address, uint8_t* data, size_t length) override;\n"
@@ -149,60 +169,82 @@ elif hal_type == "MEM":
     getSize_definition      = "size_t " + classname + "::getSize() override;\n"
 
 elif hal_type == "CPX":
-    ihal_header_file    = "ihal_cpx.h"
+    ihal_header_file    = "IHal_cpx.h"
     ihal_classname      = "IHAL_CPX"
-    start_definition    = "error_t "    + classname + "::start() override;\n"
-    cpx_get_definition  = "void* "      + classname + "::get() override;\n"
-    cpx_set_definition  = "error_t "    + classname + "::set(void* data) override;\n"
-    stop_definition     = "error_t "    + classname + "::stop() override;\n"
+    start_definition    = "sys_error_t "    + classname + "::start() override;\n"
+    cpx_get_definition  = "void* "          + classname + "::get() override;\n"
+    cpx_set_definition  = "sys_error_t "    + classname + "::set(void* data) override;\n"
+    stop_definition     = "sys_error_t "    + classname + "::stop() override;\n"
+
+elif hal_type == "PROC":
+    iprocess_header_file        = "IProcess.h"
+    iprocess_classname          = "IProcess"
+    iprocess_start_definition   = "sys_error_t "    + classname + "::start() override;\n"
+    iprocess_stop_definition    = "sys_error_t "    + classname + "::stop() override;\n"
+    iprocess_pause_definition   = "sys_error_t "    + classname + "::pause(void* data) override;\n"
+    iprocess_resume_definition  = "sys_error_t "    + classname + "::resume() override;\n"
 
 else:
-    ihal_header_file = ""
-    ihal_classname = ""
     print("Generic C++ Files Created!")
 
 # Generate the file contents using the templates and the user input
 header_content = header_template.format(
-    filename=filename,
-    filename_upper=filename.upper(),
-    classname=classname,
-    brief=brief,
-    ihal_header_file=ihal_header_file,
-    ihal_classname=ihal_classname,
-    get_definition=get_definition.replace(classname + "::", ""),
-    set_definition=set_definition.replace(classname + "::", ""),
-    connect_definition = connect_definition.replace(classname + "::", ""),
-    sendData_definition = sendData_definition.replace(classname + "::", ""),
-    receiveData_definition = receiveData_definition.replace(classname + "::", ""),
-    disconnect_definition = disconnect_definition.replace(classname + "::", ""),
-    initialize_definition = initialize_definition.replace(classname + "::", ""),
-    readData_definition = readData_definition.replace(classname + "::", ""),
-    writeData_definition = writeData_definition.replace(classname + "::", ""),
-    erase_definition = erase_definition.replace(classname + "::", ""),
-    getSize_definition = getSize_definition.replace(classname + "::", ""),
-    start_definition = start_definition.replace(classname + "::", ""),
-    cpx_get_definition = cpx_get_definition.replace(classname + "::", ""),
-    cpx_set_definition = cpx_set_definition.replace(classname + "::", ""),
-    stop_definition = stop_definition.replace(classname + "::", ""),
+    filename                =   filename,
+    filename_upper          =   filename.upper(),
+    classname               =   classname,
+    brief                   =   brief,
+    
+    abstract_header_file    = ihal_header_file if ihal_header_file != "" else iprocess_header_file,
+    abstract_classname      = ihal_classname if ihal_classname != "" else iprocess_classname,
+    
+    get_definition          = get_definition                .replace(classname + "::", ""),
+    set_definition          = set_definition                .replace(classname + "::", ""),
+    connect_definition      = connect_definition            .replace(classname + "::", ""),
+    sendData_definition     = sendData_definition           .replace(classname + "::", ""),
+    receiveData_definition  = receiveData_definition        .replace(classname + "::", ""),
+    disconnect_definition   = disconnect_definition         .replace(classname + "::", ""),
+    initialize_definition   = initialize_definition         .replace(classname + "::", ""),
+    readData_definition     = readData_definition           .replace(classname + "::", ""),
+    writeData_definition    = writeData_definition          .replace(classname + "::", ""),
+    erase_definition        = erase_definition              .replace(classname + "::", ""),
+    getSize_definition      = getSize_definition            .replace(classname + "::", ""),
+    start_definition        = start_definition              .replace(classname + "::", ""),
+    cpx_get_definition      = cpx_get_definition            .replace(classname + "::", ""),
+    cpx_set_definition      = cpx_set_definition            .replace(classname + "::", ""),
+    stop_definition         = stop_definition               .replace(classname + "::", ""),
+    
+    iprocess_header_file        = iprocess_header_file,
+    iprocess_classname          = iprocess_classname,
+    iprocess_start_definition   = iprocess_start_definition  .replace(classname + "::", ""),
+    iprocess_stop_definition    = iprocess_stop_definition   .replace(classname + "::", ""),
+    iprocess_pause_definition   = iprocess_pause_definition  .replace(classname + "::", ""),
+    iprocess_resume_definition  = iprocess_resume_definition .replace(classname + "::", ""),
 )
+
 source_content = source_template.format(
-    filename=filename,
-    classname=classname,
-    get_definition=get_definition.replace("override", ""),
-    set_definition=set_definition.replace("override", ""),
-    connect_definition = connect_definition.replace("override", ""),
-    sendData_definition = sendData_definition.replace("override", ""),
-    receiveData_definition = receiveData_definition.replace("override", ""),
-    disconnect_definition = disconnect_definition.replace("override", ""),
-    initialize_definition = initialize_definition.replace("override", ""),
-    readData_definition = readData_definition.replace("override", ""),
-    writeData_definition = writeData_definition.replace("override", ""),
-    erase_definition = erase_definition.replace("override", ""),
-    getSize_definition = getSize_definition.replace("override", ""),
-    start_definition = start_definition.replace("override", ""),
-    cpx_get_definition = cpx_get_definition.replace("override", ""),
-    cpx_set_definition = cpx_set_definition.replace("override", ""),
-    stop_definition = stop_definition.replace("override", ""),
+    filename    = filename,
+    classname   = classname,
+
+    get_definition          = get_definition            .replace("override", ""),
+    set_definition          = set_definition            .replace("override", ""),
+    connect_definition      = connect_definition        .replace("override", ""),
+    sendData_definition     = sendData_definition       .replace("override", ""),
+    receiveData_definition  = receiveData_definition    .replace("override", ""),
+    disconnect_definition   = disconnect_definition     .replace("override", ""),
+    initialize_definition   = initialize_definition     .replace("override", ""),
+    readData_definition     = readData_definition       .replace("override", ""),
+    writeData_definition    = writeData_definition      .replace("override", ""),
+    erase_definition        = erase_definition          .replace("override", ""),
+    getSize_definition      = getSize_definition        .replace("override", ""),
+    start_definition        = start_definition          .replace("override", ""),
+    cpx_get_definition      = cpx_get_definition        .replace("override", ""),
+    cpx_set_definition      = cpx_set_definition        .replace("override", ""),
+    stop_definition         = stop_definition           .replace("override", ""),
+
+    iprocess_start_definition   = iprocess_start_definition     .replace("override", ""),
+    iprocess_stop_definition    = iprocess_stop_definition      .replace("override", ""),
+    iprocess_pause_definition   = iprocess_pause_definition     .replace("override", ""),
+    iprocess_resume_definition  = iprocess_resume_definition    .replace("override", ""),
 )
 
 # Write the generated file contents to disk
