bc14d2697712f798ff9929b3a6758fcadbfc722d
kenanoglu <evren.kenanoglu@infineon.com>
2023-11-30 19:48:05 +0100

    fix: clangformat error

--- CMake/ClangFormat.cmake
+++ CMake/ClangFormat.cmake
@@ -5,40 +5,38 @@ set(CLANG_FORMAT_PATH "")
 
 function(createClangFormatTargets targetName subdirectory checkonly)
     # Find the program
-    find_program(CLANG_FORMAT "clang-format" ${CLANG_FORMAT_PATH})
+    find_program(CLANG_FORMAT "clang-format")
     message(STATUS "${CLANG_FORMAT}")
-    file(GLOB_RECURSE FILES_TO_FORMAT RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} ${subdirectory}*.cpp ${subdirectory}*.hpp ${subdirectory}*.h)
+    file(GLOB_RECURSE FILES_TO_FORMAT RELATIVE ${CMAKE_CURRENT_SOURCE_DIR} ${subdirectory}/*.cpp ${subdirectory}/*.hpp ${subdirectory}/*.h)
 
     if(${checkonly})
         set(FORMAT_CHECK_PARAMS
             --style=file
-            --output-replacements-xml
-            --sort-includes
-            --recursive
             --dry-run
             --Werror)
 
         # Define the formatting check and apply parameters as variables
+        message(STATUS "${FORMAT_CHECK_PARAMS}")
+        message(STATUS "${FILES_TO_FORMAT}")
+        message(STATUS "${CMAKE_CURRENT_SOURCE_DIR}")
         add_custom_target(${targetName}
-            COMMAND
-            ${CLANG_FORMAT}
+            COMMAND ${CLANG_FORMAT}
             ${FORMAT_CHECK_PARAMS}
             ${FILES_TO_FORMAT}
-            |grep -c '<replacement ' >/dev/null && echo "Formatting check failed" || echo "Formatting check passed"
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         )
 
     else()
         set(FORMAT_APPLY_PARAMS
             --style=file
-            --i
-            --sort-includes
-            --recursive)
+            -i)
 
         # Define a custom target for applying the formatting to all source files
+        message(STATUS "${FORMAT_APPLY_PARAMS}")
+        message(STATUS "${FILES_TO_FORMAT}")
+        message(STATUS "${CMAKE_CURRENT_SOURCE_DIR}")
         add_custom_target(${targetName}
-            COMMAND
-            ${CLANG_FORMAT}
+            COMMAND ${CLANG_FORMAT}
             ${FORMAT_APPLY_PARAMS}
             ${FILES_TO_FORMAT}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
@@ -47,9 +45,9 @@ function(createClangFormatTargets targetName subdirectory checkonly)
 endfunction()
 
 function(createClangFormatApplyTarget targetName subdirectory)
-    createClangFormatTargets(${targetName} ${subdirectory} TRUE)
+    createClangFormatTargets(${targetName} ${subdirectory} FALSE)
 endfunction()
 
 function(createClangFormatCheckTarget targetName subdirectory)
-    createClangFormatTargets(${targetName} ${subdirectory} FALSE)
+    createClangFormatTargets(${targetName} ${subdirectory} TRUE)
 endfunction()
