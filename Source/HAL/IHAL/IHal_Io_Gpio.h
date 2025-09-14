cad1661dccfccfe4d795608208b71c360c1b426a
kenanoglu <evren.kenanoglu@infineon.com>
2025-09-14 16:23:03 +0200

    Remove outdated file header comments and ensure proper header guards in IHal_Io_Gpio.h

--- Source/HAL/IHAL/IHal_Io_Gpio.h
+++ Source/HAL/IHAL/IHal_Io_Gpio.h
@@ -1,15 +1,5 @@
-/** @file       ihal.h
- *  @brief      Hardware Abstraction Layer Interface
- *  @copyright  (c) 2023- Evren Kenanoglu - All Rights Reserved
- *              Permission to use, reproduce, copy, prepare derivative works,
- *              modify, distribute, perform, display or sell this software and/or
- *              its documentation for any purpose is prohibited without the express
- *              written consent of Evren Kenanoglu.
- *  @author     Evren Kenanoglu
- *  @date       12/02/2023
- */
-#ifndef FILE_IHAL_EXTENSION_H
-#define FILE_IHAL_EXTENSION_H
+#ifndef FILE_IHAL_IO_GPIO_H
+#define FILE_IHAL_IO_GPIO_H
 
 #include "IHal.h"
 #include "System/system.h"
@@ -224,4 +214,4 @@ public:
 
 #undef INTERFACE // Should not let this roam free
 
-#endif // FILE_IHAL_H
+#endif // FILE_IHAL_IO_GPIO_H
